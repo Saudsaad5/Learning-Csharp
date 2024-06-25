eaa7a416bc9897cfb7e40ae899635f9064866dab
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-06-25 11:15:40 +0300

    I learned Vars & typecasting

---
+++ Vars&typeCast.cs
@@ -0,0 +1,19 @@
+﻿
+
+const string name = "Saud";
+int age = 23;
+double height = 176.00; 
+char symbol = '@';
+bool alive = true;
+
+Console.WriteLine("Hello " + name);
+Console.WriteLine("You're " + age + " years old");
+Console.WriteLine("Your height is " + height);
+Console.WriteLine("Is alive? " + alive);
+Console.WriteLine("Your symbol is " + symbol + ", and your name is " + name);
+double x = 22.22;
+int y = Convert.ToInt32(x);
+Console.WriteLine(y.GetType());
+      
+        
+Console.ReadLine();
