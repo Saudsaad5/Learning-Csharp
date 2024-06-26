f0c93c5b37fba6e6999742669300faa4e3e6ff96
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-06-26 10:57:33 +0300

    Math Class

---
+++ MathClass.cs
@@ -0,0 +1,33 @@
+namespace We_re_so_back
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3;
+            double y = 5;
+
+            double a = Math.Pow(x, 2);
+            double b = Math.Sqrt(x);
+            double c = Math.Abs(x);
+            double d = Math.Round(x);
+            double e = Math.Ceiling(x);
+            double f = Math.Floor(x);
+            double g = Math.Max(x, y);
+            double h = Math.Min(x, y);
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+            Console.WriteLine(e);
+            Console.WriteLine(f);
+            Console.WriteLine(g);
+            Console.WriteLine(h);
+
+
+
+
+        }
+    }
+}
