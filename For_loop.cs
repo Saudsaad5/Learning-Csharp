10719c83e2dbda7dbedfbcbbd89d1ecad5d1ab0a
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-06-27 10:36:42 +0300

    For loop in C#

---
+++ For_loop.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace We_re_so_back
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
