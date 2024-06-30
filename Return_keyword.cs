d39dc4db33598dc36e7c85af367ae9547a8f0728
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-06-30 10:32:08 +0300

    Return keyword in C#

---
+++ Return_keyword.cs
@@ -0,0 +1,29 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace We_re_so_back
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1;
+            double num2;
+            double result;
+
+            Console.WriteLine("Enter the 1st number: ");
+            num1 =Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the 2nd number: ");
+            num2 =Convert.ToDouble(Console.ReadLine());
+
+            result = Mul(num1, num2);
+
+            Console.WriteLine($"Result = {result}");
+        }
+
+        static double Mul(double num1, double num2) 
+        { 
+           return num1 * num2;
+        }
+
+    }
+}
