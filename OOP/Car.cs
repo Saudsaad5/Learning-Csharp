27dabfc5eb76e673c303dc39deacb366cd763da6
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-07-14 14:08:28 +0300

    Static in C#

---
+++ OOP/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace We_re_so_back.OOP
+{
+     class Car
+    {
+        private String model;
+        public static int numofCars;
+        public Car(String model) 
+        { 
+            this.model = model;
+            numofCars++;
+        }
+    }
+}
