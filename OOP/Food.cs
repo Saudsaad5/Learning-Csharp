38dc21605166c9805ae48547c79c9e1dd89e6dd3
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-07-14 14:17:26 +0300

    Add files via upload

---
+++ OOP/Food.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace We_re_so_back.OOP
+{
+    internal class Food
+    {
+    }
+}
