5d96134e1908e98706b1a67589fad437e28cf5d9
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-07-14 13:53:54 +0300

    Constructors in C#

---
+++ OOP/Human.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace We_re_so_back.OOP
+{
+     class Human
+    {
+        private string name;
+        private int age;
+
+        public Human(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void def()
+        {
+            Console.WriteLine($"Your name is {name} and your age is {age}");
+        }
+
+        public void work()
+        {
+            Console.WriteLine($"{name} is working!");
+        }
+    }
+}
