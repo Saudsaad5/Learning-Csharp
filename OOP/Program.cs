1c77b2ab44bffd7fedb437373d84ac0d503e63e4
Saud <123446988+Saudsaad5@users.noreply.github.com>
2024-07-14 12:22:10 +0300

    Classes in C#

---
+++ OOP/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace We_re_so_back
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Replay.Hello();
+
+            Replay.Gn();
+
+            Replay.Gm();
+
+            Replay.Bye();
+        }
+    }
+}
---
+++ OOP/Replays.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace We_re_so_back
+{
+    static class Replay
+    {
+        public static void Hello()
+        {
+            Console.WriteLine("Hello There!!");
+        }
+        
+        public static void Bye()
+        {
+            Console.WriteLine("Bye Bye!!");
+        }
+
+        public static void Gm()
+        {
+            Console.WriteLine("Good Morning!");
+        }
+
+        public static void Gn() 
+        {
+            Console.WriteLine("Good Night!");
+        }
+    }
+}
